d208634bf9b876aadb9d7c21635d36b026b5e144
MikaelAtowski <miguelcobian05@gmail.com>
2025-02-17 08:55:38 -0600

    Creation of a custom Password Encoder and usage of a Spring default Password Encoder

--- src/main/java/com/example/security/security/SecurityConfig.java
+++ src/main/java/com/example/security/security/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -49,8 +50,15 @@ public class SecurityConfig{
 		return new JdbcUserDetailsManager(dataSource);
 	}*/
 	
-	@Bean
+	/*@Bean
 	PasswordEncoder passwordEncoder() {
 	    return NoOpPasswordEncoder.getInstance();
+	}*/
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
+	
+	
 }
